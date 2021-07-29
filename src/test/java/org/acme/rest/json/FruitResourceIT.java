3cc8c381f01b91431b70a7514c03ca81a1c67bab
Mitesh Ghimire <mitesh_ghimire@outlook.com>
2021-07-29 01:17:54 -0400

    Update FruitResourceIT.java
    Removing test comment

--- src/test/java/org/acme/rest/json/FruitResourceIT.java
+++ src/test/java/org/acme/rest/json/FruitResourceIT.java
@@ -1,7 +1,7 @@
 package org.acme.rest.json;
 
 import io.quarkus.test.junit.NativeImageTest;
-// This is a test comment
+
 @NativeImageTest
 public class FruitResourceIT extends FruitResourceTest {
 
